3c5bd294827cc94cac0e4521532b5f1ca0a32eb2
Faheetah <Faheetah@users.noreply.github.com>
2018-03-28 16:56:25 -0500

    Add interface to breakit

--- breakit/breakit.go
+++ breakit/breakit.go
@@ -10,11 +10,34 @@ import (
 	"github.com/labstack/echo"
 )
 
+const indexHtml = `
+<html>
+	<body>
+		<h2>Links:</h2>
+		<h4><a href="/chronograf/" onclick="javascript:event.target.port=8888">Chronograf</a></h4>
+		<h4><a href="/grafana/"    onclick="javascript:event.target.port=3000">Grafana</a></h4>
+		<h4><a href="/golerta/"    onclick="javascript:event.target.port=5608">Golerta</a></h4>
+		<h2>Modify State:</h3>
+		<h4>Current status: %d</h4>
+		<h4>Current trend value: %02f</h4>
+		<h4><a href="/status/200">Send 200</a></h4>
+		<h4><a href="/status/404">Send 404</a></h4>
+		<h4><a href="/status/500">Send 500</a></h4>
+		<h4><a href="/trending/reset">Reset Trending</a></h4>
+    </body>
+</html>
+`
+
 func weightedRand() (n int) {
 	second := time.Now().Second()
 	n = ((second%10)/(1+rand.Intn(5))) * 100 + rand.Intn(100)
 	return
 }
+
+var (
+	base = time.Now()
+	status = 200
+)
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -22,12 +45,12 @@ func main() {
 
 	// Always return a static metric
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		d := time.Now().Sub(base)
+		t := d.Seconds() / 10
+		return c.HTML(http.StatusOK, fmt.Sprintf(indexHtml, status, t))
 	})
 
 	// Mimic status codes by pushing to the server
-	status := 200
-
 	e.GET("/status", func(c echo.Context) error {
 		r := weightedRand()
 		time.Sleep(time.Duration(r) * time.Millisecond)
@@ -36,7 +59,7 @@ func main() {
 
 	e.GET("/status/:status", func(c echo.Context) error {
 		status, _ = strconv.Atoi(c.Param("status"))
-		return c.String(http.StatusOK, "OK")
+		return c.Redirect(http.StatusTemporaryRedirect, "/")
 	})
 
 	// Random value
@@ -45,7 +68,6 @@ func main() {
 	})
 
 	// Trending
-	base := time.Now()
 	e.GET("/trending", func(c echo.Context) error {
 		d := time.Now().Sub(base)
 		t := (d.Seconds() + float64(rand.Intn(30))) / 10
@@ -54,7 +76,7 @@ func main() {
 
 	e.GET("/trending/reset", func(c echo.Context) error {
 		base = time.Now()
-		return c.String(200, "OK")
+		return c.Redirect(http.StatusTemporaryRedirect, "/")
 	})
 
 	// Seasonality
